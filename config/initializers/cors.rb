3e56edae39be5f7acb77b3419cead1b851606e00
Duggan Roberts <dugganroberts@gmail.com>
2019-08-28 14:08:05 -0500

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'young-inlet-65294.herokuapp.com'
+    origins '*'
 
     resource '*',
       headers: :any,
